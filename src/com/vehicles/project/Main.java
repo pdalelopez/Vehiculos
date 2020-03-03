04561a1410dcf79544092ca52fd7dba2c3b724bb
pdalelopez <pdalelopez@users.noreply.github.com>
2020-03-03 09:56:02 +0100

    Fase1 values inserting

--- src/com/vehicles/project/Main.java
+++ src/com/vehicles/project/Main.java
@@ -1,10 +1,25 @@
 package com.vehicles.project;
 
+import java.util.Scanner;
+
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		Car car1 = new Car();
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Insert plate: ");
+		String plate = input.next();
+		System.out.println("Insert brand: ");
+		String brand = input.next();
+		System.out.println("Insert color: ");
+		String color = input.next();
+		
+		
+		Car car1 = new Car(plate,brand,color);
+		
 
 	}
 
